a0fd6d3b3666a84c6dbb86c98e0dcba2751b42a6
aravind2003git <138103911+aravind2003git@users.noreply.github.com>
2023-07-01 20:28:24 +0530

    Add files via upload

---
+++ src/test/java/com/movie3/movie3/Movie3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movie3.movie3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Movie3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
